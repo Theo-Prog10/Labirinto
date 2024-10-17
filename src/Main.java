ce77d72ec14794955ce3395372550e76aeacfadc
eduardo-pitanga <edudupilo@gmail.com>
2024-10-16 21:40:32 -0300

    Coordenada corrigida

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Entrada input = new Entrada("labirinto 1.csv");
         Labirinto lista = new Labirinto();
         lista.setLab(input.preencher());
-        lista.setInicio(17, 1); //labirinto 1.csv
+        lista.setInicio(16, 1); //labirinto 1.csv
         //lista.setInicio(100, 63); //labirinto 2.csv
         lista.solve();
     }
